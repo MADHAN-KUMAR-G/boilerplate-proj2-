2ac2bbf7ef7abf1915f6174246673d872bace18e
mk <mk@gmail.com>
2025-10-25 19:18:28 +0530

    updated tf1

--- boilerplate2/terraform/main.tf
+++ boilerplate2/terraform/main.tf
@@ -9,80 +9,59 @@ terraform {
 
 provider "docker" {}
 
-# -----------------------------
 # Create a Docker network
-# -----------------------------
 resource "docker_network" "app_network" {
-  name = "boiler_network"
+  name = "app_network"
 }
 
-# -----------------------------
-# PostgreSQL Container
-# -----------------------------
+# Postgres container
 resource "docker_container" "postgres" {
   name  = "postgres_db"
   image = "postgres:15"
-
-  env = [
-    "POSTGRES_DB=boilerdb",
-    "POSTGRES_USER=postgres",
-    "POSTGRES_PASSWORD=postgres"
-  ]
 
   ports {
     internal = 5432
     external = 5432
   }
 
-  # ✅ Absolute host path for Jenkins-safe persistent volume
-  volumes {
-    host_path      = abspath("${path.module}/pgdata")
-    container_path = "/var/lib/postgresql/data"
-  }
+  env = [
+    "POSTGRES_USER=postgres",
+    "POSTGRES_PASSWORD=postgres",
+    "POSTGRES_DB=mydb"
+  ]
 
   networks_advanced {
     name = docker_network.app_network.name
   }
+
+  volumes {
+    host_path      = "/home/madhan/boilerplate2/terraform/pgdata"
+    container_path = "/var/lib/postgresql/data"
+  }
+
+  restart = "always"
 }
 
-# -----------------------------
-# Boiler App Container
-# -----------------------------
-resource "docker_container" "boiler_app" {
-  name  = "boiler_app"
-  image = "madhan1205/boiler:latest"
-  depends_on = [docker_container.postgres]
+# Node.js app container (pull image from Docker Hub)
+resource "docker_container" "node_app" {
+  name  = "node_app"
+  image = "madhan1205/boiler:latest"  # pull the latest image from Docker Hub
+
+  env = [
+    "PORT=3000",
+    "DATABASE_URL=postgres://postgres:postgres@postgres_db:5432/mydb"
+  ]
 
   ports {
     internal = 3000
     external = 3000
   }
 
-  env = [
-    "DB_HOST=postgres_db",
-    "DB_USER=postgres",
-    "DB_PASSWORD=postgres",
-    "DB_NAME=boilerdb",
-    "DB_PORT=5432"
-  ]
-
   networks_advanced {
     name = docker_network.app_network.name
   }
-}
 
-# -----------------------------
-# Outputs
-# -----------------------------
-output "app_container_name" {
-  value = docker_container.boiler_app.name
-}
-
-output "postgres_container_name" {
-  value = docker_container.postgres.name
-}
-
-output "app_url" {
-  value = "http://localhost:3000"
+  depends_on = [docker_container.postgres]
+  restart    = "on-failure"
 }
 
